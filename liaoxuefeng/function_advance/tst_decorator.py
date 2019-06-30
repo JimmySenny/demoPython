eb6f9194cf8c05079e2431bd78fd2ccdfe63e680
JimmySenny <JimmySenny@126.com>
2019-06-30 12:36:20 +0800

    modify decorator

--- liaoxuefeng/function_advance/tst_decorator.py
+++ liaoxuefeng/function_advance/tst_decorator.py
@@ -28,7 +28,7 @@ def log2(text):
 def now2():
 	print('decorator2');
 
-def log3(text):
+def log3(func):
 	@functools.wraps(func)
 	def wrapper(*args, **kwargs):
 		print('call %s()' % func.__name__);
@@ -44,7 +44,7 @@ def log4(text):
 		return wrapper;
 	return decorator;
 
-@log3('33333')
+@log3
 def now3():
 	print('decorator3');
 
@@ -52,26 +52,55 @@ def now3():
 def now4():
 	print('decorator4');
 
+def log4(params=None):
+	def decorator(fn):
+		@functools.wraps(fn)
+		def wrapper(*args, **kwargs):
+			print('%s begin call' % fn.__name__);
+			if(isinstance(params, str)):
+				print('args:%s'%params);
+			func = fn(*args, **kwargs);
+			print('%s end call' % fn.__name__);
+			return func;
+		return wrapper;
+	if(not callable(params)):
+		return decorator;
+	return decorator(params);
+
+@log4
+def f1():
+	print('f1 execute');
+
+@log4()
+def f2():
+	print('f2 execute');
+
+@log4('params...')
+def f3():
+	print('f3 execute');
+
+
 def main():
-	print('now');
+	print('now--------------------');
 	f = now;
 	f(); # decorator
 	print(now.__name__); # now
 	print(f.__name__);   # now
-	print('now1');
-	now1();
-	print(now1.__name__);
-	print('now2');
-	now2();
-	print(now2.__name__);
-	print('now3');
-	now3();
-	print(now3.__name__);
-	print('now4');
-	now4();
-	print(now4.__name__);
-
-
-
+	print('now1-------------------');
+	now1();	#call now1 decorator1
+	print(now1.__name__); #wrapper
+	print('now2-------------------');
+	now2();	#execute now2(): decorator2
+	print(now2.__name__); #wrapper
+	print('now3-------------------');
+	now3(); #call now3()  decorator3
+	print(now3.__name__); #now3
+	print('now4-------------------');
+	now4();	#call now4() decorator4
+	print(now4.__name__); #now4
+	print('f-------------------');
+	f1();
+	f2();
+	f3();
 if __name__ == '__main__':
 	main();
