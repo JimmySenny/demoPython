4b86f2225be5d8602179db7f89cca2bf6979ef9b
JimmySenny <JimmySenny@126.com>
2019-08-24 23:54:01 +0800

    add intermediate

---
+++ intermediate/three_cond/tst_cond.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- conding:utf-8 -*-
+
+is_fat = True;
+state = 'fat' if is_fat else 'not fat';
+print(state);
---
+++ intermediate/three_cond/tst_condtuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+fat = True;
+fitness = ("skinny", "fat")[fat]
+
+print("Ali is ", fitness);
